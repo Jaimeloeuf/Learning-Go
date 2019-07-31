caea0fa02f1ff9e43d5e646ce6c1c1f8d80c4011
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-07-31 16:52:59 +0800

    Created new file for doing square roots.

---
+++ src/sqrt.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+import "math"
+
+// Shorthand function to calculate square of a value
+var SQ = func(input float64) (output float64) { return input * input }
+
+func Sqrt(x float64) float64 {
+	fmt.Println("value of X is: ", x)
+	z := float64(1)
+
+	for ((z*z - x) / (2 * z)) < -1 {
+		z++
+		fmt.Println("test: ", (z*z-x)/(2*z))
+	}
+	return z
+
+	for ; x >= SQ(z+1); z++ {
+
+		if v := SQ(z); x > v {
+			continue
+		} else {
+			return z
+		}
+	}
+	return z
+}
+
+func main() {
+	x := 67
+
+	// Want to make sure z^2 close to X
+	fmt.Println("\nValue from my func: ", Sqrt(float64(x)))
+	fmt.Println("Value from Math lib: ", math.Floor(math.Sqrt(float64(x))))
+}
